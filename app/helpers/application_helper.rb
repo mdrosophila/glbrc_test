4a0c50d17e9a0e0ef94eb57e0ed7eb164ae1a4db
mdrosophila <qyang3@wisc.edu>
2016-09-22 20:41:18 +0000

    ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Return the full title on a pre-page basis.
+  
+  def full_title(page_title='')
+    base_title = "GLBRC test apll"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
