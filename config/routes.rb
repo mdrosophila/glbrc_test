0b1853e947e87187c1fc28c279cee517352b45f1
mdrosophila <qyang3@wisc.edu>
2016-09-26 18:49:24 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
+  # The above is a short cut for get '/help', to: 'static_pages#help'
+  get '/home', to: 'static_pages#home' #, as: 'my', use as to rename the path from home_path to my_path
   root 'static_pages#home'
+  get '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
