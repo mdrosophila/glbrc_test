c35a536c306e0160c26f4d3b2f5a7925e7eeb03e
mdrosophila <qyang3@wisc.edu>
2016-10-03 19:48:10 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  # The above is a short cut for get '/help', to: 'static_pages#help'
+  # The above is a short cut for get '/home', to: 'static_pages#home'
   get '/home', to: 'static_pages#home' #, as: 'my', use as to rename the path from home_path to my_path
   root 'static_pages#home'
   get '/signup',  to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
